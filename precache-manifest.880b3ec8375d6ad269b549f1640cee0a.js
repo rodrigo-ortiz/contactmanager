dcfb9aa288bcb4a5abf952cc52ba6e197bbff0a5
Roni Ortiz <roniortiz@MacBook-Pro-de-Roni.local>
2019-03-06 17:26:39 -0600

    Updates

---
+++ precache-manifest.880b3ec8375d6ad269b549f1640cee0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "4247148411c8caaa3444",
+    "url": "/contactmanager/static/js/main.42471484.chunk.js"
+  },
+  {
+    "revision": "25ee209c794a3b7499ea",
+    "url": "/contactmanager/static/js/2.25ee209c.chunk.js"
+  },
+  {
+    "revision": "25ee209c794a3b7499ea",
+    "url": "/contactmanager/static/css/2.270c36e5.chunk.css"
+  },
+  {
+    "revision": "eb66fb00d5f7a18d5fd3abfd6dd86b1d",
+    "url": "/contactmanager/index.html"
+  }
+];
